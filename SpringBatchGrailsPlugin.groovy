3ad50998a9bfd96723438a0ef86b257c7e8bf283
Daniel Bower <daniel.d.bower@gmail.com>
2014-11-18 14:10:50 -0500

    Update min version to Grails 2.3

--- SpringBatchGrailsPlugin.groovy
+++ SpringBatchGrailsPlugin.groovy
@@ -24,7 +24,7 @@ import org.springframework.batch.core.repository.dao.AbstractJdbcBatchMetadataDa
 
 class SpringBatchGrailsPlugin {
     def version = "2.1.0"
-    def grailsVersion = "2.0 > *"
+    def grailsVersion = "2.3 > *"
     def title = "Grails Spring Batch Plugin"
     def author = "John Engelman"
     def authorEmail = "john.r.engelman@gmail.com"
